debe4d263c59632a39ea15a970f98ce27475933c
Ishrat Jahan <jahanishratme@gmail.com>
2022-09-09 11:25:56 +1000

    Added margin shadow background border

--- src/template/doc-details.js
+++ src/template/doc-details.js
@@ -1,11 +1,27 @@
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
 import React from "react"
+import {Box} from "@mui/material";
 
-export default function DocDetails({ data }) {
+export default function DocDetails({data}) {
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
-    </div>
+    <Box sx={{
+      maxWidth: '1200px',
+      mx: 'auto',
+    }}>
+      <Box sx={{
+        px: {xs: '1rem', md: '2rem', lg: '3rem'},
+        background: '#F7F7FA',
+        border: '1px solid #E3E5F1',
+        borderRadius: '8px',
+        margin: {xs: '1rem', md: '2rem 1rem'},
+        boxShadow: `rgba(149, 157, 165, 0.2) 0px 8px 24px`,
+        "& h1,h2,h3,h4,h5,h6": {
+          color: '#60679F'
+        }
+      }}>
+        <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+      </Box>
+    </Box>
   )
 }
 
