366b5e54ab9113735c8a43703199cd52c98cffde
Tanish-22 <t.dhapola2002@gmail.com>
2022-09-25 04:45:25 +1000

    making final changes

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -1,30 +1,135 @@
 import React from "react"
-import { graphql } from 'gatsby'
+import { graphql } from "gatsby"
 import "./index.css"
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button"
 import { Link } from "gatsby"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import { StaticImage } from "gatsby-plugin-image"
+
+import { MDBFooter } from "mdb-react-ui-kit"
+
+// import Search from '@mui/icons-material/Search';
+// import SearchIconWrapper from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search"
+// import StyledInputBase from '@mui/icons-material/Search';
+
+import { styled, alpha } from "@mui/material/styles"
+import InputBase from "@mui/material/InputBase"
+
+import Grid from "@mui/material/Grid"
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}))
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}))
 
 export default function Docs({ data }) {
   const docs = data.allFile.nodes
   return (
     <div>
-      <h1>List all the docs</h1>
-      <nav>
+      <Navbar className="navbar-color" bg="grey" expand="lg">
+        <Navbar.Brand href="/">
+          <StaticImage
+            className="brandimg"
+            width={40}
+            src={"../assets/logo.png"}
+          />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home">Capstone</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end" style={{ width: "100%" }}>
+            <Nav.Link>
+              <Button className="navlink" href="#home" variant="outline-dark">
+                Home
+              </Button>
+            </Nav.Link>
+
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="heading">
+        <h1>List all the docs</h1>
+      </div>
+
+      <Grid container spacing={4}>
         {docs.map(doc => (
-
-          <Button className="docs" variant="light">
-
-            <Link key={doc.id} to={`/${doc.relativePath}`}
-              style={{ display: "block" }} >
-
-              {doc.relativePath}
-              
+          <Grid item xs={4}>
+            <Link
+              key={doc.id}
+              to={`/${doc.relativePath}`}
+              style={{ textDecoration: "none" }}
+            >
+              <div className="docs">{doc.relativePath}</div>
             </Link>
-
-          </Button>
+          </Grid>
         ))}
-      </nav>
+      </Grid>
+
+      <MDBFooter
+        bgColor="light"
+        className="text-center text-lg-start text-muted"
+      >
+        <div
+          className="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        >
+          © 2022 Copyright:
+          <a className="text-reset fw-bold">Thoth Tech</a>
+        </div>
+      </MDBFooter>
     </div>
   )
 }
@@ -44,3 +149,19 @@ export const query = graphql`
     }
   }
 `
+
+// import React from 'react';
+// import { MDBFooter } from 'mdb-react-ui-kit';
+
+// export default function App() {
+//   return (
+//     <MDBFooter bgColor='light' className='text-center text-lg-left'>
+//       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+//         &copy; {new Date().getFullYear()} Copyright:{' '}
+//         <a className='text-dark' href='https://mdbootstrap.com/'>
+//           MDBootstrap.com
+//         </a>
+//       </div>
+//     </MDBFooter>
+//   );
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,75 +1,111 @@
-import { Box } from "@mui/material"
+// import { Box } from "@mui/material"
 import React from "react"
 import "./index.css"
-import Button from 'react-bootstrap/Button';
-import { Link } from "gatsby"
+import Button from "react-bootstrap/Button"
+// import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+// import GitHubIcon from "@mui/icons-material/GitHub"
+// import { CFooter } from "@coreui/react"
+// import { CLink } from "@coreui/react"
 
-
+// import React from 'react';
+import { MDBFooter} from 'mdb-react-ui-kit';
 
 function Home() {
   return (
     <Container>
       <Navbar className="navbar-color" bg="grey" expand="lg">
-        
+        <Navbar.Brand href="/">
+          <StaticImage
+            className="brandimg"
+            width={40}
+            src={"../assets/logo.png"}
+          />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home">Capstone</Navbar.Brand>
 
-          <Navbar.Brand href="/">
-            <StaticImage className="brandimg" width={40} src={'../assets/logo.png'} />
-          </Navbar.Brand>
-          <Navbar.Brand href="#home">
-            Capstone
-          </Navbar.Brand>
-
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-
-            <Nav className="justify-content-end" style={{width: "100%"}}>
-
-              <Nav.Link>
-                <Button className="navlink" href="#home" variant="outline-dark">Home</Button>
-              </Nav.Link>
-              <Nav.Link>
-                <Button className="navlink" href="#products" variant="outline-dark">Products</Button>
-              </Nav.Link>
-              <Nav.Link>
-                <Button className="navlink" href="#getstarted" variant="outline-dark">Get Started</Button>
-              </Nav.Link>
-
-            </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end" style={{ width: "100%" }}>
+            <Nav.Link>
+              <Button className="navlink" href="#home" variant="outline-dark">
+                Home
+              </Button>
+            </Nav.Link>
+            <Nav.Link>
+              <Button
+                className="navlink"
+                href="#products"
+                variant="outline-dark"
+              >
+                Products
+              </Button>
+            </Nav.Link>
+            <Nav.Link>
+              <Button
+                className="navlink"
+                href="#getstarted"
+                variant="outline-dark"
+              >
+                Get Started
+              </Button>
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Row className="row1">
-        <Col className="col1">
-          <div className="block1">
-            <h1 className="heading">
-              Thoth Tech
-            </h1>
-            <br></br>
-            <p className="para">One stop for all projects</p>
-            
-            <Button className="btn" variant="dark">
-              <Box className="link" component={Link} to={'/docs'} variant='contained'>
-                Get Started
-              </Box>
-            </Button>
-            
-          </div>
-        </Col>
-        <Col className="col2">
-        <StaticImage width={450} src={'../assets/capstone.png'} />
-        </Col>
-      </Row>
+      <div className="test">
+        <Row className="row">
+          <Col className="col">
+            <div className="block1">
+              <h1 className="heading">Thoth Tech</h1>
+              <br></br>
+              <p className="para">One stop for all projects</p>
 
+              <a href="/docs">
+                <Button className="btn" variant="outline-dark">
+                  Explore Now
+                </Button>
+              </a>
+            </div>
+          </Col>
+          <Col className="col">
+            <StaticImage width={450} src={"../assets/capstone.png"} />
+          </Col>
+        </Row>
+      </div>
+      {/* <CFooter
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <CLink href="https://coreui.io">Thoth Tech</CLink>
+          <span> &copy; 2022</span>
+        </div>
+      </CFooter> */}
+
+
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
       
-      
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2022 Copyright: 
+        <a className='text-reset fw-bold'>
+           Thoth Tech
+        </a>
+      </div>
+    </MDBFooter>
+
     </Container>
   );
 }
-export default Home;
+export default Home
