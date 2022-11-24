4a9a97b69f3c436ec01243b374ad61eeb1ed195a
Rtam22 <kauza22@hotmail.com>
2022-11-25 10:11:06 +1100

    Added file naming and type filter

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,10 @@ const express = require("express")
 const multer = require("multer")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-// const simpleGit = require("simple-git")
 const shelljs = require("shelljs")
+const path = require("path")
 const app = express()
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -12,18 +13,30 @@ app.use(
 )
 const port = process.env.PORT || 5000
 
-const upload = multer({ dest: "uploads" })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  },
+})
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    const ext = path.extname(file.originalname)
+    if (ext !== ".md") {
+      return cb(new Error("Markdown files only.."))
+    }
+    cb(null, true)
+  },
+})
 
 app.post("/upload", upload.single("file"), async (req, res) => {
-  console.log(req.file)
-  shelljs.cd(
-    `C:\\Users\\RT\\Documents\\Internal-systems-offical\\Internal-Systems`
-  )
-  let test = shelljs.exec(`git add src/server/uploads`)
-  console.log(test)
+  shelljs.exec(`git add uploads`)
   shelljs.exec(`git commit -m "Uploaded file" `)
   shelljs.exec(`git push origin main`)
-  shelljs.cd(`src\\server`)
 })
 
 app.listen(port, () => console.log("server started on port 5000"))
