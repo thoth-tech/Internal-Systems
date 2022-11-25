4dc89c1e53f10c049b84096d4f3b4fbc89afc28d
Rtam22 <kauza22@hotmail.com>
2022-11-25 13:08:36 +1100

    Refactored search into component and cleanup

---
+++ src/Components/UploadDocument.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+function UploadDocument() {
+  const [file, setFile] = useState()
+
+  const send = () => {
+    const data = new FormData()
+    data.append("file", file)
+    axios
+      .post("http://localhost:5000/upload", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <form action="#" enctype="multipart/form-data">
+      <div>
+        <label htmlFor="fileUpload">File</label>
+        <input
+          type="file"
+          id="file"
+          onChange={event => {
+            const file = event.target.files[0]
+            setFile(file)
+          }}
+        />
+      </div>
+      <button onClick={send}>Upload</button>
+    </form>
+  )
+}
+
+export default UploadDocument
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,9 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import NavBar from "../Components/NavBar"
 import Footer from "../Components/Footer"
-import axios from "axios"
+import UploadDocument from "../Components/UploadDocument"
 
 function Home() {
-  const [file, setFile] = useState()
-
-  const send = () => {
-    const data = new FormData()
-    data.append("file", file)
-    axios
-      .post("http://localhost:5000/upload", data)
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-  }
   return (
     <Container>
       <NavBar />
@@ -35,20 +25,7 @@ function Home() {
               <Button href="/docs" className="btn" variant="outline-dark">
                 Explore Now
               </Button>
-              <form action="#" enctype="multipart/form-data">
-                <div>
-                  <label htmlFor="fileUpload">File</label>
-                  <input
-                    type="file"
-                    id="file"
-                    onChange={event => {
-                      const file = event.target.files[0]
-                      setFile(file)
-                    }}
-                  />
-                </div>
-                <button onClick={send}>Upload</button>
-              </form>
+              <UploadDocument />
             </div>
           </Col>
           <Col className="col">
