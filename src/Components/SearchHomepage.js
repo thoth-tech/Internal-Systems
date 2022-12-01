816af103dbfb541750e78c9d9a564375ac253543
Auden Vitasa <abvitasa@gmail.com>
2022-12-01 11:11:14 +0800

    update search-results page UI and NavBar component

--- src/Components/SearchHomepage.js
+++ src/Components/SearchHomepage.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
 import * as JsSearch from "js-search"
+import NavBar from "./NavBar"
+import {
+  Box,
+  InputBase,
+  List,
+  ListItem,
+  Paper,
+  Typography,
+} from "@mui/material"
+
+import SearchIcon from "@mui/icons-material/Search"
+import { IconButton } from "gatsby-theme-material-ui"
+import { Container } from "@mui/system"
 
 class Search extends Component {
   state = {
@@ -55,30 +68,75 @@ class Search extends Component {
     }
 
     return (
-      <div>
-        <br />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="Search">Enter your search here</label>
-          <input
-            value={queryValue}
-            onChange={this.searchData}
-            ref={ref => ref && ref.focus()}
-            placeholder="Enter your search here"
-          />
-        </form>
-        Number of results: {queryResults.length}
-        <hr />
-        {queryResults.map(item => {
-          return (
-            <div key={`row_${item.id}`}>
-              <Link to={`/${item.relativePath}`}>{item.title}</Link>
-              <br />
-              <label>{item.relativePath}</label>
-              <hr />
-            </div>
-          )
-        })}
-      </div>
+      <>
+        <NavBar />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mt: 5,
+          }}
+        >
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: "78%",
+            }}
+            onSubmit={this.handleSubmit}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search"
+              value={queryValue}
+              fullWidth
+              onChange={this.searchData}
+              inputRef={input => input && input.focus()}
+            />
+
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Box>
+        <Container sx={{ width: "80%" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography color="text.secondary" variant="body2">
+              Number of results: {queryResults.length}
+            </Typography>
+            <List component="nav" aria-label="mailbox folders">
+              {queryResults.map(item => {
+                return (
+                  <ListItem
+                    button
+                    divider
+                    key={`row_${item.id}`}
+                    onClick={() => navigate(`/${item.relativePath}`)}
+                    sx={{ pt: 3 }}
+                  >
+                    <Box sx={{ flexDirection: "column" }}>
+                      <Typography color="info.main" variant="subtitle1" d>
+                        {`${item.title}`.toUpperCase()}
+                      </Typography>
+                      <Typography color="text.secondary" variant="body2" d>
+                        {item.relativePath}
+                      </Typography>
+                    </Box>
+                  </ListItem>
+                )
+              })}
+            </List>
+          </Box>
+        </Container>
+      </>
     )
   }
 }
