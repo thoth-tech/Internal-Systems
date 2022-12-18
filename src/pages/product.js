93aeae431105590eff4b9b26db06372b3fbb36e2
Arshdeep Sokhal <arsh@Arshdeeps-MacBook-Pro.local>
2022-12-18 11:01:32 +1100

    Upadated Product page

--- src/pages/product.js
+++ src/pages/product.js
@@ -10,95 +10,103 @@ import Col from "react-bootstrap/Col"
 import NavBar from "../Components/NavBar"
 import Footer from "../Components/Footer"
 
-function Home() {
+function AutoLayoutExample() {
   return (
     <Container>
       <NavBar />
-      <div className="test">
-     
-          <h1 className="heading" >Our Products</h1>
-          <br></br>
-          <br></br>
-          
-            
-       
-       
+
+      <h1 className="heading" >Our Products</h1>
+      <br></br>
+      <br></br>
+      <div className="test1">
 
         <Row className="row">
-          <Col className="col">
-            <div className="block1">
-              <a href = "/ontrack"><h2 className="heading">Ontrack</h2></a>
-              <br></br>
-            </div>
-          </Col>
-          <Col><StaticImage
+          <Col>
+            <a href="/ontrack"><StaticImage className="p-img"
               width={220}
               src={"../assets/doubtfire.png"}
               alt="capstone"
-            />
+            /></a>
           </Col>
-            
+          <Col className="col">
+            <div className="block1">
+              <a href="/ontrack"><h2 className="heading">Ontrack</h2></a>
+              <br></br>
+            </div>
+          </Col>
+
+
         </Row>
 
         <Row className="row">
+
+          <Col>
+            <a href="/splashkit"> <StaticImage className="p-img"
+              width={220}
+              src={"../assets/splashkit.png"}
+              alt="capstone" href="/splashkit"
+            /></a>
+          </Col>
           <Col className="col">
             <div className="block1">
               <a href="/splashkit"><h2 className="heading">Splashkit</h2></a>
               <br></br>
             </div>
           </Col>
-          <Col>
-            <StaticImage
-              width={220}
-              src={"../assets/splashkit.png"}
-              alt="capstone" href="/"
-            /></Col>
-            
+
         </Row>
         <Row className="row">
-          <Col className="col">
-            <div className="block1">
-              <a href ="/dreambig"><h2 className="heading">Dreambig</h2></a>
-              <br></br>
-            </div>
-          </Col>
+
           <Col>
-            <StaticImage
+            <a href="/dreambig">  <StaticImage className="p-img"
               width={220}
               src={"../assets/qwert.png"}
               alt="capstone"
-            />
-           </Col>
-            
-        </Row>
-        <Row className="row">
+            /></a>
+
+          </Col>
           <Col className="col">
             <div className="block1">
-              <a href ="/companyops"><h2 className="heading">Company-Operation</h2></a>
+              <a href="/dreambig"><h2 className="heading">Dreambig</h2></a>
               <br></br>
             </div>
           </Col>
-          <Col>
-            <StaticImage
+
+        </Row>
+        <Row className="row">
+          <Col className="col">
+            <a href="/companyops"> <StaticImage className="p-img"
               width={220}
               src={"../assets/company-operation-background.png"}
               alt="companyoperation"
-            />
-           </Col>
-            
+            /></a>
+            <br></br>
+
+
+
+          </Col>
+          <Col>
+            <div className="block1">
+              <a href="/companyops"><h2 className="heading">Company-Operation</h2></a>
+              <br></br>
+            </div>
+
+          </Col>
+
         </Row>
 
-        <Row className="row">
-          <Col className="col">
-         < p className="para">One stop for all projects</p>
-              <Button href="https://github.com/thoth-tech" className="btn" variant="outline-dark">
-               Company Github
-              </Button>
-          </Col>
-        </Row>
+
       </div>
+      <Row className="row">
+        <Col className="col">
+          < p className="para">One stop for all projects</p>
+          <Button href="https://github.com/thoth-tech" className="btn" variant="outline-dark">
+            Company Github
+          </Button>
+        </Col>
+      </Row>
       <Footer />
     </Container>
   )
 }
-export default Home
+export default AutoLayoutExample;
